4d9037e8fa7a5cf38e554f81ef9fd7c10cbcecb1
bitey-mouse <rpflamion@gmail.com>
2020-10-17 12:51:38 -0500

    Added migration step to Procfile for heroku deployment

---
+++ expo-app/Components/AddressInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+import './AddressInput.css';
+
+
+const addressInput = props => {
+    return (
+        <TouchableOpacity style={{borderWidth: 1, color: 'light grey', placeholderTextColor: 'light grey' }}>
+ 
+        </TouchableOpacity>
+    )
+};
+
+export default addressInput;
